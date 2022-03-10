9f5744eb9f3dd7c9c7d2075394d22b4306f012ba
Stef Claus <stef.claus@gmail.com>
2022-03-10 02:45:45 +0000

    add stg prefix to files

---
+++ greenery/models/staging/postgres/stg_users.sql
@@ -0,0 +1,9 @@
+select 
+    user_id as user_guid
+    first_name,
+    last_name,
+    phone_number,
+    created_at as created_at_utc,
+    updated_at as created_at_utc,
+    address_id as address_at_utc
+from {{ source('src_postgres', 'users')}}
