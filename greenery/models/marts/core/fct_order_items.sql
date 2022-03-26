4b4f0b7e66f2a32662cf3f77f4ca719ce3a74f71
Michael Caruana <caruana.mc@gmail.com>
2022-03-26 17:58:33 +0000

    Fixing names in this model

--- greenery/models/marts/core/fct_order_items.sql
+++ greenery/models/marts/core/fct_order_items.sql
@@ -15,7 +15,7 @@ select
   orders.created_at,
   order_items.product_id,
   products.product_name,
-  sum(order_items.num_items) as total_items
+  sum(order_items.num_items) as num_items
 from {{ ref('stg_orders') }} orders
 left join {{ ref('stg_order_items') }} order_items
   on orders.order_id = order_items.order_id
