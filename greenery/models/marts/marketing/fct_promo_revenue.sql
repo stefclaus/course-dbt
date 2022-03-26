4d388ef8dd31798e6650d0cd4ee4526b65d29fc2
Michael Caruana <caruana.mc@gmail.com>
2022-03-26 02:16:43 +0000

    Refactor marketing mart SQL

--- greenery/models/marts/marketing/fct_promo_revenue.sql
+++ greenery/models/marts/marketing/fct_promo_revenue.sql
@@ -1,27 +1,21 @@
-  {{
-  config(
-    materialized='table'
-  )
-}}
-  
-  with order_item_agg as (
+with order_item_agg as (
   select
     order_id,
     sum(num_items) as total_items
-  from
-    {{ ref('fct_order_items') }}
+  from {{ ref('fct_order_items') }}
   group by 1
-  )
-  select
-    o.promo_name,
-    sum(o.order_cost) as total_promo_revenue,
-    sum(i.total_items) as total_promo_items
-  from
-    {{ ref('fct_orders') }} o
-  inner join
-    order_item_agg i
-  on
-    o.order_id = i.order_id
-  where
-    o.promo_name is not null
-  group by 1
+  ),
+
+orders as (
+  select * from {{ ref('stg_orders')}}
+)
+
+select
+  orders.promo_name,
+  sum(orders.order_cost) as total_promo_revenue,
+  sum(order_item_agg.total_items) as total_promo_items
+from orders
+inner join order_item_agg
+  on orders.order_id = order_item_agg.order_id
+where orders.promo_name is not null
+group by 1
