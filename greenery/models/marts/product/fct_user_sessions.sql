d13ab0ea11f8369385954d6052a3f2e9d70c64a5
Michael Caruana <caruana.mc@gmail.com>
2022-03-26 02:35:21 +0000

    Refactoring user session fact table further

--- greenery/models/marts/product/fct_user_sessions.sql
+++ greenery/models/marts/product/fct_user_sessions.sql
@@ -10,13 +10,18 @@ with sessions as (
 users as (
   select * from {{ ref('stg_users')}}
 )
+
 select
-    s.user_id as user_id,
-    count(sessions.session_id) as num_sessions
+    sessions.user_id as user_id,
+    min(sessions.session_start) as first_visit,
+    max(sessions.session_end) as last_vist,
+    date_part('day',current_timestamp - max(sessions.session_end)) as days_since_last_session,
+    count(sessions.session_id) as num_sessions,
     avg(sessions.session_length) as avg_session_length,
     sum(sessions.num_page_views) as total_page_views,
     sum(sessions.num_cart_adds) as total_cart_adds,
     sum(sessions.num_checkouts) as total_checkouts 
 from sessions
 left join users
-on s.user_id = users.user_id
+  on sessions.user_id = users.user_id
+group by 1
