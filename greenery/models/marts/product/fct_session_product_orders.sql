dafc6dd4c3c0439c494eafffdad26e8c64591ebb
Stef Claus <stef.claus@gmail.com>
2022-03-27 01:26:01 +0000

    write fct session product orders

--- greenery/models/marts/product/fct_session_product_orders.sql
+++ greenery/models/marts/product/fct_session_product_orders.sql
@@ -1,32 +1,30 @@
-
-
-
-  with order_items as (select product_guid, 
+with order_session as(
+  select session_guid, 
     order_guid,
-    quantity
-  from {{ ref('stg_order_items') }}),
+    quantity, 
+    checkout,
+    package_shipped
+  from {{ ref('int_order_session') }}
+  where checkout=true),
   
-  sessions as (
-    select session_guid, 
-      order_guid, 
-      product_guid
-    from {{ ref('stg_events') }}),
+product_session as (
+  select session_guid, 
+    name, 
+    add_to_cart,
+    page_view
+  from {{ ref('int_product_session') }})
     
-product_order as (
-    select a.product_guid,
-    a.order_guid,
-    a.quantity,
-    b.session_guid
-  from order_items a
-  full outer join sessions b
-  on a.order_guid=b.order_guid
-  and b.product_guid=b.product_guid)
+select  b.name,
+  a.session_guid, 
+  a.order_guid, 
+  a.quantity,
+  a.checkout,
+  a.package_shipped,
+  b.add_to_cart,
+  b.page_view
+from order_session a
+left outer join product_session b
+on a.session_guid=b.session_guid
+
+
   
-  select a.product_guid,
-    a.order_guid,
-    a.quantity,
-    a.session_guid,
-    b.name
-  from product_order a
-  left outer join {{ ref('stg_products') }} b
-  on a.product_guid=b.product_guid
--- greenery/models/marts/product/int/int_order_session.sql
+++ greenery/models/marts/product/int/int_order_session.sql
@@ -1,23 +1,22 @@
-with order_items as (select product_guid, 
-    order_guid,
-    quantity
-  from {{ ref('stg_order_items') }}),
-  
-  sessions as (
+with sessions as (
     select session_guid, 
       order_guid, 
       product_guid,
       event_type
     from {{ ref('stg_events') }}
-    where order_guid is not null) 
+    where order_guid is not null),
 
- select a.product_guid,
+order_items as (
+  select product_guid, 
+    order_guid,
+    quantity
+  from {{ ref('stg_order_items') }})
+  
+ select a.session_guid,
     a.order_guid,
-    a.quantity,
-    b.session_guid,
-    case when b.event_type = 'checkout' then true else false end as checkout,
-    case when b.event_type = 'package_shipped' then true else false end as package_shipped
-  from order_items a
-  left outer join sessions b
+    case when a.event_type = 'checkout' then true else false end as checkout,
+    case when a.event_type = 'package_shipped' then true else false end as package_shipped,
+    b.quantity
+  from sessions a
+  left join order_items b
   on a.order_guid=b.order_guid
-  and b.product_guid=b.product_guid
