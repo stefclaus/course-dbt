d06ee45111475c002b63d8e5c6ad7cf70b27f46c
Stef Claus <stef.claus@gmail.com>
2022-04-05 15:54:48 +0000

    add exposures.yml

---
+++ greenery/snapshots/orders.sql
@@ -0,0 +1,15 @@
+{% snapshot orders_snapshot %}
+
+  {{
+    config(
+      target_schema='snapshots',
+      unique_key='order_id',
+
+      strategy='timestamp',
+      updated_at='estimated_delivery_at',
+    )
+  }}
+
+  SELECT * FROM {{ source('greenery','orders') }}
+
+{% endsnapshot %}
